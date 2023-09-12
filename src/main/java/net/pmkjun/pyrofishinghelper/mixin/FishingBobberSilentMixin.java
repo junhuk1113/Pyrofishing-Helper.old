4f88cd6a4da7d3d271cc624b2e100f928d945912
junhyeok <junhuk1113@naver.com>
2023-09-12 21:05:04 +0900

    다른 사람 낚시찌가 첨벙거리는 소리 음소거

--- src/main/java/net/pmkjun/pyrofishinghelper/mixin/FishingBobberSilentMixin.java
+++ src/main/java/net/pmkjun/pyrofishinghelper/mixin/FishingBobberSilentMixin.java
@@ -1,21 +1,41 @@
 package net.pmkjun.pyrofishinghelper.mixin;
 
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.NetworkThreadUtils;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.listener.TickablePacketListener;
 import net.minecraft.network.packet.s2c.play.PlaySoundFromEntityS2CPacket;
 import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
 import net.minecraft.sound.SoundEvents;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
-public abstract class FishingBobberSilentMixin {
+public abstract class FishingBobberSilentMixin implements TickablePacketListener, ClientPlayPacketListener {
 
-    @Inject(method = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;onPlaySound(Lnet/minecraft/network/packet/s2c/play/PlaySoundS2CPacket;)V",at=@At("RETURN"))
-    public void onPlaySoundFromEntity(PlaySoundS2CPacket packet, CallbackInfo cir){
-        System.out.println(packet.getSound().getKey().get().getValue().getPath());
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public void onPlaySound(PlaySoundS2CPacket packet) {
+        NetworkThreadUtils.forceMainThread(packet, this, this.client);
+        if(packet.getSound().getKey().get().getValue().getPath().equals("entity.fishing_bobber.splash")){
+            System.out.println("sound ignored");
+            return;
+        }
+        this.client.world.playSound(this.client.player, packet.getX(), packet.getY(), packet.getZ(), packet.getSound(), packet.getCategory(), packet.getVolume(), packet.getPitch(), packet.getSeed());
     }
 
 
